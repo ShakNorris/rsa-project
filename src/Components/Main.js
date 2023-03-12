bfaf68e8883035b1d73104edf7785b1c25573f45
Shako Kushashvili <shako.kushashvili@gmail.com>
2023-03-12 00:58:09 -0500

    Added P & Q generator

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Switch, FormControlLabel, FormGroup, TextField,Button } from '@mui/material';
 import CalculateIcon from '@mui/icons-material/Calculate';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
 
 import './Main.css'
 
@@ -8,17 +9,51 @@ function Main() {
     const [inputCount, setInputCount] = useState(0);
     const [outputCount, setOutputCount] = useState(0);
     const [check, setCheck] = useState(false);
+    const [P, setP] = useState();
+    const [Q, setQ] = useState();
 
-    const handleChange = () => {
+    const changeCheck = () => {
         setCheck((e) => !e);
     };
+
+    const getPrimes = (min, max) => {
+        const result = Array(max + 1)
+          .fill(0)
+          .map((_, i) => i);
+        for (let i = 2; i <= Math.sqrt(max + 1); i++) {
+          for (let j = i ** 2; j < max + 1; j += i) delete result[j];
+        }
+        return Object.values(result.slice(Math.max(min, 2)));
+      };
+      
+      const getRandNum = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      
+      const getRandPrime = (min, max) => {
+        const primes = getPrimes(min, max);
+        return primes[getRandNum(0, primes.length - 1)];
+      };
+
+      const generatePnQ = () => {
+        setQ(getRandPrime(1,1000));
+        setP(getRandPrime(1,1000));
+      }
+
+      const handleChangeP = (event) => {
+        setP(event.target.value);
+      };
+
+      const handleChangeQ = (event) => {
+        setQ(event.target.value);
+      };
 
   return (
     <div>
         <div className='toggle'>
             <FormGroup className='toggle'>
                 <FormControlLabel
-                control={<Switch check={check} onChange={handleChange} />}
+                control={<Switch check={check} onChange={changeCheck} />}
                 label={`${check ? 'Decrypt':'Encrypt'}`}
                 />
             </FormGroup>
@@ -34,14 +69,20 @@ function Main() {
 
             <div className='keys'>
                 <div className='prime'>
-                    <TextField id="outlined-basic" label="P" variant="outlined" />
-                    <TextField id="outlined-basic" label="Q" variant="outlined" />
+                    <TextField id="P" label="P" variant="outlined" focused value={P} onChange={handleChangeP}/>
+                    <TextField id="Q" label="Q" variant="outlined" focused value={Q} onChange={handleChangeQ}/>
                     <Button variant="contained" startIcon={<CalculateIcon/>}
                     style={{
                         height:50,
                         backgroundColor:'#60b0f4',
                         fontWeight: "bold",
-                    }}>Calculate</Button>   
+                    }}>Calculate Keys</Button>
+                    <Button onClick={generatePnQ} variant="contained" startIcon={<SettingsSuggestIcon/>}
+                    style={{
+                        height:50,
+                        backgroundColor:'#60b0f4',
+                        fontWeight: "bold",
+                    }}>Generate P & Q</Button>   
                 </div>  
                 <div className='key-values'>
                     <TextField
